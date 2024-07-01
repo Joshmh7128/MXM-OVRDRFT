d3b6856191367d9c29efbac8ab4bb0b102bd330b
Josh Hirshfield <joshmh7128@gmail.com>
2024-06-30 21:39:27 -0400

    map updates

--- Assets/Scripts/CarAudioController.cs
+++ Assets/Scripts/CarAudioController.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CarAudioController : MonoBehaviour
 {
     // our engine
     [SerializeField] AudioSource engineSource;
-    [SerializeField] float currentPitch; // our current pitch is equal to the current speed of the car
+    [SerializeField] AudioClip accel, idle;
     CarController carController;
-
+    [SerializeField] float gearMax, gearCurrentProgress, speed, bounceAmount;
+    [SerializeField] int currentGear; // our current gear and how much more we move up when we start a new gear
     private void Start()
     {
         // get our car controller
         carController = GetComponent<CarController>();
+        engineSource.clip = accel;
+        gearMax = accel.length;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         // process our engine
         ProcessEngine();
@@ -23,12 +27,84 @@ public class CarAudioController : MonoBehaviour
 
     void ProcessEngine()
     {
-        currentPitch = Mathf.MoveTowards(currentPitch, carController.moveInput * 3, 1f * Time.deltaTime);
 
-        engineSource.pitch = currentPitch;
+        if (!engineSource.isPlaying)
+        {
+            engineSource.Play();
+        }
 
-        // if we reach our max pitch, shift up
-        if (currentPitch >= carController.moveInput * 3)
-            currentPitch -= Random.Range(0.1f, 0.2f);
+        // if we ever hit the top of the gear, bounce on it
+        if (gearCurrentProgress >= gearMax)
+        {
+            // if we're not drifting shift up
+            if (!carController.drifting)
+                ShiftUp();
+            // if we are drifting...
+            else
+            {
+                gearCurrentProgress -= bounceAmount;
+                engineSource.time = gearCurrentProgress;
+            }
+        }
+
+        // while accelerating, play the accel clip. when we decel, move backwards through the clip, then replay at the same point one accel
+        if (carController.moveInput > 0)
+        {
+            gearCurrentProgress += speed * Time.deltaTime;
+            engineSource.pitch = speed * 1;
+            engineSource.clip = accel;
+            // engineSource.time = gearCurrent;
+        }
+
+        // if we're decelerating
+        if (carController.moveInput <= 0)
+        {
+            gearCurrentProgress -= Time.deltaTime;
+            engineSource.pitch = -1;
+        }
+
+        if (gearCurrentProgress <= 0)
+        {
+            // if we are not in 1st gear, shift down
+            if (currentGear != 0)
+            {
+                ShiftDown();
+            }
+
+            gearCurrentProgress = 0.1f;
+        }
+
+        // if we're low and idling
+        if (gearCurrentProgress < 0.5f && carController.moveInput < 0.1)
+        {
+        }
+
+        if (engineSource.pitch == 0)
+        {
+            engineSource.pitch = 0.1f;
+        }
+    }
+
+    // shift up
+    void ShiftUp()
+    {
+
+        // reduce our gearCurrent time by our current gear * the ratio increase
+        gearCurrentProgress = gearMax * 0.5f + (currentGear * 0.1f);
+
+        currentGear++;
+        // set the time
+        engineSource.time = gearCurrentProgress;
+    }
+
+    void ShiftDown()
+    {
+        // reduce our gearCurrent time by our current gear * the ratio increase
+        gearCurrentProgress = gearMax;
+        currentGear = 0;
+
+        // set the time
+        engineSource.time = gearCurrentProgress;
     }
 }
+
